d2ae3e9ce6c3853232648742acc7b4f7d3d5f720
adrianetodesign <adrianetodesign@gmail.com>
2022-01-20 23:24:32 +0000

    Fixed error messages, and added test cases.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,18 @@
 const request = require('request');
 
-const fetchBreedDescription = function (breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (err, response, body) => {
-  if (err) {
-    return callback(err, null);
-  }
-  const data = JSON.parse(body);
-  if (data[0] === undefined) {
-    return callback(null, 'Sorry, that breed has not been found.');
-  }
-  if (data[0] !== undefined) {
-    return callback(null, data[0].description);
-  }
-});
-
-}
+    if (err) {
+      return callback(err, null);
+    }
+    const data = JSON.parse(body);
+    if (data[0] === undefined) {
+      return callback(new Error("Breed not found"), null);
+    }
+    if (data[0] !== undefined) {
+      return callback(null, data[0].description);
+    }
+  });
+};
 
 module.exports = { fetchBreedDescription };
